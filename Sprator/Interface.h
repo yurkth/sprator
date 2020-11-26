03f7d2db8aaf7dfb63e6ca519aad4106bdcb853b
torin <yurkth@gmail.com>
2020-11-27 01:38:47 +0900

    Updated Interface class

--- Sprator/Interface.h
+++ Sprator/Interface.h
@@ -15,19 +15,55 @@ namespace sprator {
         int exportSize = 400;
 
         // UI
+        struct UISettings {
+            Vec2 pos;
+            int width = 0;
+            int maxChars = 0;
+        };
         const HSV darkGray = HSV(0, 0, 0.2);
         const Font label = Font(20, Typeface::Medium);
+
+        const Rect generateBG = Rect(600, 5, 195, 216);
+        const UISettings generateLabel = { generateBG.pos + Vec2(52, 5) };
+        const UISettings seedLabel = { generateBG.pos + Vec2(11, 35) };
+        const UISettings seedTextBox = { generateBG.pos + Vec2(63, 71), 120, 8 };
+        const UISettings loadButton = { generateBG.pos + Vec2(12, 120), 171 };
+        const UISettings refreshButton = { generateBG.pos + Vec2(12, 168), 171 };
+
+        const Rect exportBG = Rect(600, 226, 195, 353);
+        const UISettings exportLabel = { exportBG.pos + Vec2(65, 5) };
+        const UISettings baseLabel = { exportBG.pos + Vec2(11, 35) };
+        const UISettings baseRadio = { exportBG.pos + Vec2(35, 61), 155 };
+        const UISettings baseTextBox = { exportBG.pos + Vec2(89, 100), 94, 6 };
+        const UISettings bgLabel = { exportBG.pos + Vec2(11, 142) };
+        const UISettings sharpLabel = { exportBG.pos + Vec2(70, 182) };
+        const UISettings bgTextBox = { exportBG.pos + Vec2(89, 178), 94, 6 };
+        const UISettings sizeLabel = { exportBG.pos + Vec2(11, 220) };
+        const UISettings pxLabel = { exportBG.pos + Vec2(160, 260) };
+        const UISettings sizeTextBox = { exportBG.pos + Vec2(87, 256), 68, 4 };
+        const UISettings saveButton = { exportBG.pos + Vec2(12, 305), 171 };
+
         const Font copyright = Font(10, Typeface::Regular);
-        const Rect GenerationMenu = Rect(600, 5, 195, 216);
-        const Rect ExportMenu = Rect(600, 226, 195, 353);
+        const UISettings copyrightLabel = { Vec2(727, 581) };
+
+        bool textBox(TextEditState& tes, const UISettings& settings, bool enabled = true) {
+            return SimpleGUI::TextBox(tes, settings.pos, settings.width, settings.maxChars, enabled);
+        }
+        bool button(const String& text, const UISettings& settings) {
+            return SimpleGUI::Button(text, settings.pos, settings.width);
+        }
+        bool radioButtons(const Array<String>& options, const UISettings& settings) {
+            return SimpleGUI::RadioButtons(baseIndex, options, settings.pos, settings.width);
+        }
 
         void parseColor(HSV& targetColor, TextEditState& targetTes, double brightness);
-        void updateSprites(Array<sprator::Sprite>& sprites, size_t baseIndex, HSV& baseColor, HSV& bgColor);
-        void updateSprites(Array<sprator::Sprite>& sprites, size_t baseIndex, HSV& baseColor, HSV& bgColor, int growCount);
+        void updateSprites(Array<sprator::Sprite>& sprites);
+        void updateSprites(Array<sprator::Sprite>& sprites, int growCount);
 
         void drawSprites(Array<sprator::Sprite>& sprites);
         void drawGenerateMenu(Array<sprator::Sprite>& sprites);
         void drawExportMenu(Array<sprator::Sprite>& sprites);
+
     public:
         Interface();
         int seed = Random(0, 99999999);
